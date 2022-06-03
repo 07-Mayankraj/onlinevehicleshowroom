181a59380ca8a30c97f39bc8d160fd91fccfe011
Mayank raj <87657007+07-Mayankraj@users.noreply.github.com>
2022-06-03 19:03:21 +0530

    Update footer.php

--- admin_common/footer.php
+++ admin_common/footer.php
@@ -1,5 +1,5 @@
  <div id="footer-sec">
-        &copy; 2017 Vehicle Website | Developed By: Manvendra Chauhan
+        &copy; 2017 Vehicle Website | Developed By: Mayank raj
     </div>
     <!-- /. FOOTER  -->
   
